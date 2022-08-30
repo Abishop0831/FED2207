4001aeb63ae41f46bcf005d6f7c13beb1f29caea
alex <abishop0831@gmail.com>
2022-08-30 07:55:06 -0400

    updates

---
+++ Projects/Twitter Clone/dashboard.js
@@ -0,0 +1,11 @@
+/* 
+let div = document.createElement("div");
+let p = document.createElement("p");
+let span = document.createElement("span");
+div.append(p);
+div.prepend(span);
+
+console.log(div.childNodes); 
+for posting fn 
+
+*/
---
+++ Projects/Twitter Clone/signup.js
@@ -0,0 +1,41 @@
+let signUpForm = document.getElementById("mobileForm");
+
+
+
+ function User (name, email, username, password) {
+ this.name = name;
+ this.email = email;
+ this.username = username;
+ this.password = password;
+ this.age = "above 13";   
+}
+
+
+
+
+signUpForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let name = signUpForm.querySelector('input[name="name"]').value
+    let email = signUpForm.querySelector('input[name="email"]').value
+    let password = signUpForm.querySelector('input[name="password"]').value
+    let username = signUpForm.querySelector('input[name="username"]').value
+    console.log(new User(name, email, username, password));
+})
+
+
+
+/* 
+users = {
+name: e.name,
+password: e.password,
+ email: e.email,
+ age:e.age,
+ username: e.username, 
+}
+ 
+}
+*/
+ 
+ // let create = new user(e.target.name, e.target.password,)
+  
+  
